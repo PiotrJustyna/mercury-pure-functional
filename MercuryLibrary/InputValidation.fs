39425d74fb1c3e282e97aaed27625766116f7827
Piotr Justyna <piotr.justyna@gmail.com>
2021-09-17 14:56:00 +0100

    Input validation input types made explicit.

--- MercuryLibrary/InputValidation.fs
+++ MercuryLibrary/InputValidation.fs
@@ -2,6 +2,6 @@ module MercuryLibrary.InputValidation
 
 open System
 
-let whoisInputValidation apiUrlFormat domain =
+let whoisInputValidation (apiUrlFormat: string) (domain: string) =
     if String.IsNullOrWhiteSpace apiUrlFormat then raise (new ArgumentException(nameof(apiUrlFormat))) else ()
     if String.IsNullOrWhiteSpace domain then raise (new ArgumentException(nameof(domain))) else ()
