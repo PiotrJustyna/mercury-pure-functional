58da3a7d9a4e0ac30073c34ba9f86cf1b9647dc5
Piotr Justyna <piotr.justyna@gmail.com>
2021-09-20 14:16:24 +0100

    Unit tests finished.

--- MercuryLibrary/Mappers.fs
+++ MercuryLibrary/Mappers.fs
@@ -7,23 +7,23 @@ let toWhoisResponse (domain: string) (whoisRecord: WhoisRecord) =
     let now = DateTime.UtcNow
 
     let createdDate =
-        match System.DateTime.TryParse whoisRecord.createdDate with
+        match DateTime.TryParse whoisRecord.createdDate with
         | _, date -> date
 
     let updatedDate =
-        match System.DateTime.TryParse whoisRecord.updatedDate with
+        match DateTime.TryParse whoisRecord.updatedDate with
         | _, date -> date
 
     let expiresDate =
-        match System.DateTime.TryParse whoisRecord.expiresDate with
+        match DateTime.TryParse whoisRecord.expiresDate with
         | _, date -> date
 
     let auditCreatedDate =
-        match System.DateTime.TryParse whoisRecord.audit.createdDate with
+        match DateTime.TryParse whoisRecord.audit.createdDate with
         | _, date -> date
 
     let auditUpdatedDate =
-        match System.DateTime.TryParse whoisRecord.audit.updatedDate with
+        match DateTime.TryParse whoisRecord.audit.updatedDate with
         | _, date -> date
 
     { Domain = domain
