888eacd273bde45fa74e7b905140e814c7cdd879
Piotr Justyna <piotr.justyna@gmail.com>
2021-10-01 16:33:26 +0100

    Updated input validation + its exceptions.

--- MercuryLibrary/InputValidation.fs
+++ MercuryLibrary/InputValidation.fs
@@ -3,5 +3,5 @@ module MercuryLibrary.InputValidation
 open System
 
 let whoisInputValidation (apiUrlFormat: string) (domain: string) =
-    if String.IsNullOrWhiteSpace apiUrlFormat then raise (new ArgumentException(nameof(apiUrlFormat))) else ()
-    if String.IsNullOrWhiteSpace domain then raise (new ArgumentException(nameof(domain))) else ()
+    if String.IsNullOrWhiteSpace apiUrlFormat then invalidArg (nameof apiUrlFormat) ""
+    if String.IsNullOrWhiteSpace domain then invalidArg (nameof domain) ""
